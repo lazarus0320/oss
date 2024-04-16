5106b56bd1fac7eeeef52fee348c92a201997142
larzarus0320 <astar5327z@gmail.com>
2024-04-16 10:36:22 +0900

    fix: fixed name2

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='stringformatter0320',
-    version='0.0.2',
+    name='stringformatter03201',
+    version='0.0.1',
     description='string formatter by eggSushi0320',
     author='eggSushi0320',
     author_email='astar5327z@gmail.com',
     url='https://github.com/lazarus0320/oss.git',
     install_requires=['',],
     packages=find_packages(exclude=[]),
-    keywords=['stringformatter0320'],
+    keywords=['stringformatter03201'],
     python_requires='>=3.6',
     package_data={},
     zip_safe=False,
