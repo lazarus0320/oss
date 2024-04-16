7da4e2f01a575e152c35a5f5fb94bec8181d9498
larzarus0320 <astar5327z@gmail.com>
2024-04-16 11:10:25 +0900

    feat: version up

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='stringformatter03201',
-    version='0.0.1',
+    version='0.0.2',
     description='string formatter by eggSushi0320',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='eggSushi0320',
     author_email='astar5327z@gmail.com',
     url='https://github.com/lazarus0320/oss.git',
-    install_requires=['',],
+    install_requires=[],
     packages=find_packages(exclude=[]),
     keywords=['stringformatter03201'],
     python_requires='>=3.6',
-    package_data={},
-    zip_safe=False,
     classifiers=[
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
